75d4895cebb4b630a906d540bf0a58878f819e78
Natalia Francisca <nathifrancisca@gmail.com>
2022-11-14 10:45:23 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const data = require('./db/data.json');
+
+app.use(cors({
+    origin: "*",
+    methods: ['GET']
+}))
+
+app.get("/members", (req, res) => {
+    if(!data.results){
+        res.status(503).json({message: "Something wrong with the request, try later"})
+    }else{
+        res.json(data.results).status(200);
+    }
+})
+
+app.get("/members/:member", (req, res) => {
+    const findMember = data.results.find(member => member.email === req.params.member);
+
+    if(!findMember){
+        res.status(404).json({message: "Member not found"});
+    }else{
+        res.json(findMember).status(200);
+    }
+})
+
+app.listen(9999, () => {
+    console.log("🔥 Server is running at port: 9999")
+})
